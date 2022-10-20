14e897f0bef7b16a562f5f66fa463621bcd5f5d2
AllanEl <allanrnelias@gmail.com>
2022-10-20 19:50:35 -0400

    logged ingredients into console

--- assets/main.js
+++ assets/main.js
@@ -28,57 +28,45 @@ var lowerCase = [
 ];
 
 function getDrinks() {
-  var ingredientName = document.getElementById("searchBar").value
-  var requestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + ingredientName;
-
+  var ingredientName = document.getElementById("searchBar").value;
+  var requestUrl =
+    "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" +
+    ingredientName;
 
   fetch(requestUrl)
-    .then(function(response) {
+    .then(function (response) {
       return response.json();
     })
-    .then(function(data) {
-      
-      
-
-      renderDrinks(data)
-      
+    .then(function (data) {
+      renderDrinks(data);
     });
-
 }
-var drinkBox = document.querySelector("#drinkContainer")
+var drinkBox = document.querySelector("#drinkContainer");
 var showDrinks = document.querySelector("#getDrinks");
 showDrinks.addEventListener("click", getDrinks);
 
 function renderDrinks(drinkData) {
-  console.log({drinkData})
+  console.log({ drinkData });
   drinkBox.innerHTML = "";
-  for(var i = 0 ; i < drinkData.drinks.length ; i++){
-  
-  // create
-  
-  var drinkName = document.createElement("p")
-  // modify
-
-  drinkName.textContent = drinkData.drinks[i].strDrink;
-  // append
-  document.getElementById('drinkContainer').append(drinkName)
-}}
-
+  for (var i = 0; i < drinkData.drinks.length; i++) {
+    // create
+    var drinkName = document.createElement("p");
+    // modify
+    drinkName.textContent = drinkData.drinks[i].strDrink;
+    // append
+    document.getElementById("drinkContainer").append(drinkName);
+  }
+}
 
 // Second API (Project requires at least 2)
 
-// var requestUrl = 'https://{Url}';
+var requestUrl = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list";
 
-// fetch(requestUrl)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log('Github Repo Issues \n----------');
-//     console.log(data);
-//     for( var i = 0; i < data.length ; i++){
-
-//     }console.log(data[i])
-
-
-//   });
+fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log("List of ingredients");
+    console.log(data);
+  });
